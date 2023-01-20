4a2e66f0dffe2c32386c728f9a280512e8f2ec82
Petar Milanović <116819818+PetarMilanovic@users.noreply.github.com>
2023-01-20 17:20:04 +0100

    Update stringici-zvjezdice.c

--- stringici-zvjezdice.c
+++ stringici-zvjezdice.c
@@ -1,40 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAX 1000
 
 
-void promijeni(char * tren, char * niska)
+void funkcija(char *niska,const char *stara,const char *nova)
 {
-  FILE * izlaz = fopen("izlaz.txt", "a");
-  char trenutni[strlen(tren)];
+    char *poz, temp[MAX];
+    int indeks = 0;
+    int duzina;
+    duzina= strlen(stara);
 
-  strcpy(trenutni, tren);
-  
-  while(strstr(trenutni, niska))
-  {
-    char * p = strstr(trenutni, niska);
-    for(int i = 0; i < strlen(niska); i++)
-      p[i] = '*';
-  }
+    while ((poz=strstr(niska, stara))!=NULL)
+    {
 
-  fputs(trenutni, izlaz);
-  fclose(izlaz);
+        strcpy(temp,niska);
+        indeks = poz-niska;
+        niska[indeks]='\0';
+        strcat(niska,nova);
+        strcat(niska,temp+indeks+duzina);
+    }
 }
 
 
-int main(){
-  
-  FILE *f = fopen("ulaz.txt", "r");
-  
-  char * n2 = "burek";
-  char buf[80];
-  
-  while(!feof(f)){
-    fgets(buf, 80, f);
-    promijeni(buf, n2);
-  }
-  
-  fclose(f);
-  
-  return 0;
-} 
+int main(int argc, char *argv[])
+{
+
+    FILE *f1;
+    FILE *f2;
+    char bafer[MAX];
+    char stara[100], nova[100];
+
+    f1 = fopen(argv[1],"r");
+    if(f1 == NULL) {
+      perror("Greska u f1");
+      return(-1);
+    }
+
+    f2 = fopen(argv[2],"w");
+    if(f2 == NULL) {
+      perror("Greska u f2");
+      return(-1);
+    }
+
+    strcpy(stara,argv[3]);
+
+    int m=strlen(stara);
+    for(int i=0;i<m;i++){
+        nova[i]='*';
+    }
+
+    if (f1==NULL || f2==NULL)
+    {
+        printf("Greska u unosu datoteke.\n");
+        return (-1);
+    }
+
+    while ((fgets(bafer,MAX,f1))!=NULL)
+    {
+
+        funkcija(bafer,stara,nova);
+        fputs(bafer, f2);
+    }
+
+    fclose(f1);
+    fclose(f2);
+
+    printf("Uspjesno zamijenili nisku '%s' sa'%s'.", stara, nova);
+
+    return 0;
+}
